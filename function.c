94ff0df0294773328faa7206837973b50f3a043e
RosarioGle <8824@holbertonstudents.com>
2024-11-25 13:10:07 +0000

    creation des fonctions print_decimal et _recur_int dans le fichier function.c

--- function.c
+++ function.c
@@ -1,7 +1,60 @@
 #include "main.h"
 
 /**
+ * _recur_int - print an integer with recursion
+ *
+ * @n: the integer to print
+ */
+
+void _recur_int(int n)
+{
+	unsigned int a;
+
+	a = n;
+	if (a / 10)
+	{
+		_recur_int(a / 10);
+	}
+	_putchar(a % 10 + '0');
+}
+
+/**
+ * print_decimal - print a decimal
+ *
+ * @speci: the list
+ *
+ * Return: the number of character printed
+ */
+
+int print_decimal(va_list speci)
+{
+	int len = 1;
+	int cpy;
+	unsigned int n;
+
+	n = va_arg(speci, int);
+	cpy = n;
+	if (cpy < 0)
+	{
+		_putchar('-');
+		cpy = -cpy;
+		n = cpy;
+		len++;
+	}
+	for (; n > 9; n /= 10)
+	{
+		len++;
+	}
+	_recur_int(cpy);
+	return (len);
+}
+
+/**
  * print_percentage - print the percentage chracter
+ *
+ * @speci: the list
+ *
+ * Return: the number of character printed
  */
 
 int print_percentage(va_list speci __attribute__((unused)))
