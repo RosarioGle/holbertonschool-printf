d74395918e2fdf92d7b0423b6d93c1c5f0e76d8f
RosarioGle <8824@holbertonstudents.com>
2024-11-26 14:14:32 +0000

    ajout des fonctions print_decimal et _recur_int dans le fichier function.c et ajout des prototype des fonctions print_decimal et _recur_int dans le fichier main.h

--- function.c
+++ function.c
@@ -11,3 +11,46 @@ int print_percentage(va_list speci __attribute__((unused)))
 	_putchar('%');
 	return (len);
 }
+
+/**
+ * print_decimal - print a decimal
+ *
+ * @speci: the list
+ *
+ * Return: the number of character printed
+ */
+
+int print_decimal(va_list speci)
+{
+	int len = 1;
+	int cpy;
+	cpy = va_arg(speci, int);
+	if (cpy < 0)
+	{
+		cpy = -cpy;
+		_putchar('-');
+		len++;
+	}
+	_recur_int(cpy);
+	while (cpy > 9)
+	{
+		cpy = cpy / 10;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _recur_int - print an integer with recursion
+ *
+ * @n: the integer to print
+ */
+
+void _recur_int(int n)
+{
+        if (n / 10)
+        {
+                _recur_int(n / 10);
+        }
+        _putchar(n % 10 + '0');
+}
