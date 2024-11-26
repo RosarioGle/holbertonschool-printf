5d9ca51608e53f22098bbdf4d76554e95f9dd64b
RosarioGle <8824@holbertonstudents.com>
2024-11-26 14:06:46 +0000

    deplacement de la fonction get_spe dans le fichier _printf.c et supression du fichier get_spe.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,46 @@
 #include "main.h"
 
 /**
+ * get_spe - selects the corresponding function
+ *
+ * @format: the specifier string
+ * @speci: the list
+ *
+ * Return: the numer of character printed
+ */
+
+int get_spe(const char *format, va_list speci)
+{
+	int a;
+	int len = 0, b = 0;
+	spe_t spc[] = {
+		{'%', print_percentage},
+		{'\0', NULL}
+	};
+	while (format != NULL && format[b] != '\0')
+	{
+		if (format[b] == '%' && (format[b + 1] != 'K' && format[b + 1] != '!'))
+		{
+			b++;
+			a = 0;
+			while (spc[a].spe != '\0')
+			{
+				if (spc[a].spe == format[b])
+					len += spc[a].f(speci);
+				a++;
+			}
+		}
+		else
+		{
+			_putchar(format[b]);
+			len++;
+		}
+		b++;
+	}
+	return (len);
+}
+
+/**
  * _printf - print all
  *
  * @format: the character string
