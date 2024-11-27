fc9be66408abb90502d7718acca1058e5baaaa5b
turtle-nest <n78693107@gmail.com>
2024-11-27 16:00:36 +0100

    MODIFICATION DE L'IMPLÉMENTATION. La fonction get_spe n'est plus utilisée. Les instructions de recherche d'un spécificateur sont ainsi directement contenues dans la fonction _printf.

--- _printf.c
+++ _printf.c
@@ -3,20 +3,53 @@
 /**
  * _printf - a function that simulates printf
  *
- * @format: a string of characters
+ * @format: the character string
  *
- * Return: the number of characters printed
+ * Return: the number of character printed
  */
 
 int _printf(const char *format, ...)
 {
-	int length = 0;
 	va_list ap;
+	int count = 0, i = 0, j;
+	handler_t specifiers[] = { // specifiers is an array of handler_t structures
+		{'%', print_percentage},
+		{'c', print_char},
+		{'s', print_string},
+		{'\0', NULL} // indicates the end
+	};
 
 	va_start(ap, format);
+
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	length = get_specifier(format, ap);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++; // check character after %
+			j = 0;
+			while (specifiers[j].specifier != '\0' && specifiers[j].specifier != format[i])
+				j++;
+
+				if (specifiers[j].specifier == format[i]) // if the specifier matches, the associated function is called
+					count += specifiers[j].function(ap);
+				else
+            	{
+                write(1, "%", 1);
+                write(1, &format[i], 1);
+                count += 2;
+            	}
+		}
+		else // the character is not %
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+
 	va_end(ap);
-	return (length);
+	return (count);
 }
