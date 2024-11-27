2d5f38d55bc2d908215072231016afb6200a4a18
RosarioGle <8824@holbertonstudents.com>
2024-11-27 08:22:35 +0000

    ajout des specifieur dans le fonction get_spe dans le fichier _printf.c

--- _printf.c
+++ _printf.c
@@ -14,6 +14,8 @@ int get_spe(const char *format, va_list speci)
 	int a;
 	int len = 0, b = 0;
 	spe_t spc[] = {
+		{'c', print_char},
+		{'s', print_string},
 		{'%', print_percentage},
 		{'d', print_decimal},
 		{'i', print_decimal},
