a5d43951c1951641640b2ada42b3a757a4a0fe2c
turtle-nest <n78693107@gmail.com>
2024-11-28 11:20:27 +0100

    Définition de la fonction get_specifier : la fonction get_specifier est en dehors des instructions de _printf pour éviter une fonction avec plus de 40 lignes.

--- _printf.c
+++ _printf.c
@@ -1,6 +1,56 @@
 #include "main.h"
 
 /**
+ * get_specifier - checks for a specifier
+ * and selects the corresponding function
+ *
+ * @format: the character string
+ * @ap: a variadic list of arguments
+ *
+ * Return: the number of character printed
+ */
+/* Iterate through the format string */
+
+int get_specifier(const char *format, va_list ap)
+{
+	int count = 0, i = 0, j;
+	handler_t specifiers[] = {
+		{'%', print_percentage},
+		{'c', print_char},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	while (format[i] != '\0')
+	{
+		/* Check if the current character is '%' */
+		if (format[i] == '%')
+		{
+			i++; /* Check the character after '%' */
+			j = 0;
+
+			/* Search for the matching specifier in the specifiers array */
+			while (specifiers[j].specifier != '\0' && specifiers[j].specifier != format[i])
+				j++;
+
+				/* If the specifier matches, the associated function is called */
+				if (specifiers[j].specifier == format[i])
+					count += specifiers[j].function(ap);
+		}
+		/* If the character is not a '%', write it directly */
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		/* Move to the next character in the format string */
+		i++;
+	}
+
+	return (count);
+}
+
+/**
  * _printf - a function that simulates printf
  *
  * @format: the character string
@@ -10,54 +60,16 @@
 
 int _printf(const char *format, ...)
 {
+	int count = 0;
 	va_list ap;
-	int count = 0, i = 0, j;
-	handler_t specifiers[] = {
-		{'%', print_percentage},
-		{'c', print_char},
-		{'s', print_string},
-		{'\0', NULL}
-	};
 
 	va_start(ap, format);
 
-	// Check for null format string or invalid case of a single '%'
+	/* Check for null format string or invalid case of a single '%' */
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-	// Iterate through the format string
-	while (format[i] != '\0')
-	{
-		// Check if the current character is '%'
-		if (format[i] == '%')
-		{
-			i++; // check character after %
-			j = 0;
-
-			// Search for the matching specifier in the specifiers array
-			while (specifiers[j].specifier != '\0' && specifiers[j].specifier != format[i])
-				j++;
-
-				// if the specifier matches, the associated function is called
-				if (specifiers[j].specifier == format[i])
-					count += specifiers[j].function(ap);
-				else
-            	{
-				// Handle invalid specifier by printing '%' and the character
-                write(1, "%", 1);
-                write(1, &format[i], 1);
-                count += 2;
-            	}
-		}
-		// If the character is not a '%', write it directly
-		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
-		// Move to the next character in the format string
-		i++;
-	}
+	count = get_specifier(format, ap);
 
 	va_end(ap);
 	return (count);
